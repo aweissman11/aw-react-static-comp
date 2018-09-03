9818d52e6a5301d0b89db7ab0a0b7e4ca5219806
Aaron Weissman <weissman.aaron@gmail.com>
2018-09-03 10:43:07 -0600

    first commit in my repo

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Header.css';
+// import title from 'title';
 
 const Header = () => {
   return (
     <div className='Header'>
+      <a src='#'>Write a story</a>
+      <h1>Material</h1>
+      <a src='#'>Sign in/Sign up</a>
       {/* What goes in here? */}
     </div>
   )
